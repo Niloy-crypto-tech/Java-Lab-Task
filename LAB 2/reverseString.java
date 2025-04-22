c11e48058b3033940888f0dc800396410df42390
Niloy <nilniloy2735@gmail.com>
2025-04-23 04:08:37 +0600

    Add Task1.java

---
+++ LAB 2/reverseString.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+
+public class reverseString {
+    public static void main(String[] args)
+    {
+        Scanner stdin = new Scanner(System.in);
+
+        System.out.println("Enter string : ");
+        String info = stdin.nextLine();
+
+        StringBuilder sb = new StringBuilder(info);
+        System.out.println("reverse string : " + sb.reverse());
+
+    }
+
+}
